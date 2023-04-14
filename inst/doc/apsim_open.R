fe75ffbec46835357a451d201069a4ebad3c8349
Adrian A. Correndo <correndo@ksu.edu>
2023-04-14 03:50:02 +0000

    version 2.0.3

--- inst/doc/apsim_open.R
+++ inst/doc/apsim_open.R
@@ -10,25 +10,25 @@ library(dplyr)
 library(purrr)
 library(tidyr)
 
-## ----warning=FALSE, message=FALSE---------------------------------------------
-# Obtaining filepath from package folder
-apsim_out_filepath <- system.file("extdata/soybean.out", package = "metrica")
+## ----warning=FALSE, message=FALSE, include=TRUE, echo=TRUE, eval=FALSE--------
+#  # Obtaining filepath from package folder
+#  apsim_out_filepath <- system.file("extdata/soybean.out", package = "metrica")
+#  
+#  # Use import_apsim_out for APSIM Classic output
+#  soybean.out <- metrica::import_apsim_out(filepath = apsim_out_filepath)
+#  
+#  head(soybean.out)
 
-# Use import_apsim_out for APSIM Classic output
-soybean.out <- metrica::import_apsim_out(filepath = apsim_out_filepath)
-
-head(soybean.out)
-
-## ----warning=FALSE, message=FALSE---------------------------------------------
-# Obtaining path from package folder
-apsim_db_folderpath <- system.file("extdata", package = "metrica")
-
-# Use import_apsim_db for APSIM NextGeneration output
-soybean.db <- metrica::import_apsim_db(filename = "soybean.example.db", folder = apsim_db_folderpath)
-
-head(soybean.db)
-
-# If observed.data is already as a dataframe, the user may do the match using a simple code like this:
-# PO.dataframe <- simulated.data %>% left_join(., observed.data) *by = "col" arg. could be required*
-
+## ----warning=FALSE, message=FALSE, include=TRUE, echo=TRUE, eval=FALSE--------
+#  # Obtaining path from package folder
+#  apsim_db_folderpath <- system.file("extdata", package = "metrica")
+#  
+#  # Use import_apsim_db for APSIM NextGeneration output
+#  soybean.db <- metrica::import_apsim_db(filename = "soybean.example.db", folder = apsim_db_folderpath)
+#  
+#  head(soybean.db)
+#  
+#  # If observed.data is already as a dataframe, the user may do the match using a simple code like this:
+#  # PO.dataframe <- simulated.data %>% left_join(., observed.data) *by = "col" arg. could be required*
+#  
 
